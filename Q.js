42dfe9d8aa525b4b95ff10083555eb5de46f61bc
yang <281627989@qq.com>
2015-04-10 11:41:42 +0800

    add

--- Q.js
+++ Q.js
@@ -1,6 +1,7 @@
 ;(function(win){
 	var doc=win.document,
 			Q=function(select){return selector(select);},
+			cacheDom={},
       extend=function(object,obj,prop){
         (!prop)&&(prop=obj,obj=object);
         for(var i in prop){
@@ -64,9 +65,8 @@
     	},
     	// 检测class的值是不是已经存在
     	checkClass=function(value){
-        var r=new RegExp('(\\s|^)'+value+'(\\s|$)'),
-            d=this;
-        return r.test(d.className);
+        var r=new RegExp('(\\s|^)'+value+'(\\s|$)');
+        return r.test(this[0].className);
     	},
     	// 添加class
     	addClass=function(elem,value){
@@ -93,7 +93,261 @@
           var r=new RegExp('(\\s|^)' + value);
           obj.className=Q.trim(obj.className.replace(r,''));
         }
+    	},
+    	/** 生成唯一标识 **/
+    	uid=function(){
+        return 'Q'+(Math.random()+'').slice(-8);
+    	},
+    	// 设置和获取dom的UID bool为真则强制获取
+    	setDomUid=function(elem,bool){
+        return (bool || !elem.uid)?(elem.uid=uid()):elem.uid;
+    	},
+    	// 设置dom缓存
+    	setData=function(elem,key,value){
+        var uid=setDomUid(elem);
+        if(!cacheDom[uid]){
+          cacheDom[uid]={};
+        }
+        cacheDom[uid][key]=value;
+    	},
+      // 读取dom缓存
+      getData=function(elem,key){
+        var uid=setDomUid(elem),
+            result=cacheDom[uid]||undefined;
+        return (result===undefined)?result:(result[key]!=undefined)?result[key]:undefined;
+    	},
+      // 删除数据缓存
+      deleteData=function(elem,key){
+        var uid=setDomUid(elem),
+            result=cacheDom[uid]||undefined;
+        if(result){
+          if(key){
+            result[key] && delete cacheDom[uid][key];
+          }else{
+            delete this.cacheDom[uid];
+          }
+        }
     	};
+  var touch=(function(){
+    var elemObj=null,
+        eventList={},
+        body=doc.body,
+        lastTapTime=NaN,
+        propertyList=['pagex','pageY','clientX','clientY','screenX','screenY','targets'],
+        getOwnPropertyNames=function(obj){
+          return Object.getOwnPropertyNames(obj);
+        },
+        touchStart=function(elem,callback){
+          elemObj.addEventListener('touchstart',callback,false);
+        },
+        touchEnd=function(e){
+          var events=null;
+          (function(touches){
+            var i=0,o,gesture,touch;
+            for(;o=touches[i++];){
+              gesture=eventList[o.identifier];
+              if(!gesture)continue;
+              (gesture.handle)&&(
+                clearTimeout(gesture.handle),
+                gesture.handle=null
+              );
+              (gesture.status=='taping')&&(
+                events=doc.createEvent('HTMLEvents'),
+                events.initEvent('tap',true,true),
+                propertyList.forEach(function(v){
+                  (v=='targets')?(events[v]=e.target):(events[v]=o[v]);
+                }),
+                elemObj.dispatchEvent(events)
+              );
+              (gesture.status=='swipeing')&&(
+                events=doc.createEvent('HTMLEvents'),
+                events.initEvent('swipeend',true,true),
+                propertyList.forEach(function(v){
+                  (v=='targets')?(events[v]=e.target):(events[v]=o[v]);
+                }),
+                elemObj.dispatchEvent(events)
+              );
+              (gesture.status=='holding')&&(
+                events=doc.createEvent('HTMLEvents'),
+                events.initEvent('holdend',true,true),
+                propertyList.forEach(function(v){
+                  (v=='targets')?(events[v]=e.target):(events[v]=o[v]);
+                }),
+                elemObj.dispatchEvent(events)
+              );
+              delete eventList[o.identifier];
+            }
+          }(e.changedTouches));
+          (getOwnPropertyNames(eventList).length==0)&&(
+            body.removeEventListener('touchend',touchEnd,false),
+            body.removeEventListener('touchmove',touchMove,false)
+          );
+        },
+        touchMove=function(e){
+          e.preventDefault();
+          var events=null;
+          ;(function(touchs){
+            var i=0,o;
+            for(;o=touchs[i++];){
+              (function(touch){
+                var gesture=eventList[touch.identifier];
+                if(!gesture)return;
+                var displacementX=touch.clientX-gesture.record.clientX,
+                    displacementY=touch.clientY-gesture.record.clientY,
+                    direction={
+                      y:(displacementY<0?true:false),
+                      x:(displacementX<0?false:true)
+                    },
+                    distance=Math.sqrt(Math.pow(displacementX,2)+Math.pow(displacementY,2));
+                (gesture.status=="taping" && distance>10)&&(
+                  gesture.status="swipeing",
+                  events=doc.createEvent('HTMLEvents'),
+                  events.initEvent('swipestart',true,true),
+                  propertyList.forEach(function(e){
+                    (v=='targets')?(events[v]=e.target):(events[e]=touch[e]);
+                  }),
+                  elemObj.dispatchEvent(events)
+                );
+                (gesture.status=='swipeing')&&(
+                  events=doc.createEvent('HTMLEvents'),
+                  events.initEvent('swipemove',true,true),
+                  propertyList.forEach(function(e){
+                    (v=='targets')?(events[v]=e.target):(events[e]=touch[e]);
+                  }),
+                  events.displacementX=displacementX,
+                  events.displacementY=displacementY,
+                  events.direction=direction,
+                  elemObj.dispatchEvent(events)
+                );
+              }(o));
+            }
+          }(e.changedTouches));
+        },
+        initStart=function(e){
+          var events=null;
+          (getOwnPropertyNames(eventList).length==0)&&(
+            body.addEventListener('touchend',touchEnd,false),
+            body.addEventListener('touchmove',touchMove,false)
+          );
+          (function(touches){
+            for(var i=0,o;o=touches[i++];){
+              (function(obj){
+                var touchRecord={},gesture=null;
+                for(var i in obj){
+                  touchRecord[i]=obj[i];
+                }
+                gesture={
+                  record:touchRecord,
+                  time:Date.now(),
+                  status:'taping',
+                  handle:setTimeout(function(){
+                    (gesture.status=='taping')&&(
+                      gesture.status='holding',
+                      events=doc.createEvent('HTMLEvents'),
+                      events.initEvent('hold',true,true),
+                      propertyList.forEach(function(v){
+                        (v=='targets')?(events[v]=e.target):(events[v]=touchRecord[v]);
+                      }),
+                      elemObj.dispatchEvent(events)
+                    );
+                    gesture.handle=null;
+                  },200)
+                };
+                eventList[obj.identifier]=gesture;
+              }(o));
+            }
+          }(e.changedTouches));
+        };  
+    return {
+      tap:function(elem,callback){
+        elemObj=(typeof elem!=='string')?elem:(doc.querySelector(elem));
+        touchStart(elemObj,initStart);
+        var handleCallback=function(e){
+          e.preventDefault();
+          callback.call(this,e);
+        };
+        setData(elemObj,'tap',handleCallback);
+        var fn=getData(elemObj,'tap');
+        elemObj.addEventListener('tap',fn,false);
+      },
+      doubletap:function(elem,callback){
+        var doubletapCallback=function(e){
+          if(+Date.now()-lastTapTime<500){
+            var events=doc.createEvent('HTMLEvents');
+            events.initEvent('doubletap',true,true);
+            propertyList.forEach(function(v){
+              events[v]=e[v];
+            });
+            elemObj.dispatchEvent(events);
+          }
+          lastTapTime=Date.now();
+        },
+        handleCallback=function(e){
+          e.preventDefault();
+          callback.call(this,e);
+        };
+        this.tap(elem,doubletapCallback);
+        setData(elemObj,'doubletap',handleCallback);
+        var fn=getData(elemObj,'doubletap');
+        elemObj.addEventListener('doubletap',fn,false);
+      },
+      hold:function(elem,callback){
+        elemObj=(typeof elem!=='string')?elem:(doc.querySelector(elem));
+        touchStart(elemObj,initStart);
+        var handleCallback=function(e){
+          e.preventDefault();
+          callback.call(this,e);
+        };
+        setData(elemObj,'hold',handleCallback);
+        var fn=getData(elemObj,'hold');
+        elemObj.addEventListener('hold',fn,false);
+      },
+      holdend:function(elem,callback){
+        elemObj=(typeof elem!=='string')?elem:(doc.querySelector(elem));
+        touchStart(elemObj,initStart);
+        var handleCallback=function(e){
+          e.preventDefault();
+          callback.call(this,e);
+        };
+        setData(elemObj,'holdend',handleCallback);
+        var fn=getData(elemObj,'holdend');
+        elemObj.addEventListener('holdend',fn,false);
+      },
+      swipestart:function(elem,callback){
+        elemObj=(typeof elem!=='string')?elem:(doc.querySelector(elem));
+        touchStart(elemObj,initStart);
+        var handleCallback=function(e){
+          e.preventDefault();
+          callback.call(this,e);
+        };
+        setData(elemObj,'swipestart',handleCallback);
+        var fn=getData(elemObj,'swipestart');
+        elemObj.addEventListener('swipestart',fn,false);
+      },
+      swipemove:function(elem,callback){
+        elemObj=(typeof elem!=='string')?elem:(doc.querySelector(elem));
+        touchStart(elemObj,initStart);
+        var handleCallback=function(e){
+          e.preventDefault();
+          callback.call(this,e);
+        };
+        setData(elemObj,'swipemove',handleCallback);
+        var fn=getData(elemObj,'swipemove');
+        elemObj.addEventListener('swipemove',fn,false);
+      },
+      swipeend:function(elem,callback){
+        elemObj=(typeof elem!=='string')?elem:(doc.querySelector(elem));
+        touchStart(elemObj,initStart);
+        var handleCallback=function(e){
+          e.preventDefault();
+          callback.call(this,e);
+        };
+        setData(elemObj,'swipeend',handleCallback);
+        var fn=getData(elemObj,'swipeend');
+        elemObj.addEventListener('swipeend',fn,false);
+      }
+    };
+  }());
 	var result={
 		v:1.0,
 		each:function(obj,callback,args){
@@ -119,6 +373,10 @@
         return r;
      	}
     },
+    // 检测class的值是不是已经存在
+    hasClass:function(value){
+      return checkClass.call(this,value);
+    },
     // 移除属性
     removeAttr:function(name){
       removeAttr(this[0],name);
@@ -133,6 +391,66 @@
     removeClass:function(value){
       removeClass(this[0],value);
       return this;
+    },
+    // 数据缓存读和写
+    data:function(key,value){
+      var elem=this[0],
+      		result;
+      if(key&&value){
+      	setDomUid(elem);
+      	setData(elem,key,value);
+      	return;
+      }else if(key){
+      	result=getData(elem,key);
+      	return result;
+      }
+    },
+    // 数据缓存删除
+    deleteData:function(key){
+    	var elem=this[0];
+    	if(!key){
+    		deleteData(elem);
+    	}else{
+    		deleteData(elem,key);
+    	}
+    	return this;
+    },
+    bind:function(type,callback){
+      var fn=null;
+      switch(type){
+        case 'tap':(//点触事件
+          touch.tap(this[0],callback)
+        );break;
+        case 'doubletap':(//双击事件
+          touch.doubletap(this[0],callback)
+        );break;
+        case 'hold':(//长按事件
+          touch.hold(this[0],callback)
+        );break;
+        case 'holdend':(//长按结束时事件
+          touch.holdend(this[0],callback)
+        );break;
+        case 'swipestart':(//开始滑动事件
+          touch.swipestart(this[0],callback)
+        );break;
+        case 'swipemove':(//滑动中的事件
+          touch.swipemove(this[0],callback)
+        );break;
+        case 'swipeend':(//滑动结束事件
+          touch.swipeend(this[0],callback)
+        );break;
+        default:(//其它事件
+          setData(this[0],type,callback),
+          fn=getData(this[0],type),
+          this[0].addEventListener(type,fn,false)
+        );break;
+      }
+    },
+    unbind:function(type){
+      (type=='doubletap')&&(arguments.callee.call(this,'tap'));
+      var fn=getData(this[0],type);
+      this[0].removeEventListener(type,fn,false);
+      deleteData(this[0],type);
     }
 	};
 	Q.extend=function(obj,prop){
--- README.md
+++ README.md
@@ -15,6 +15,7 @@ QM.js已废弃。
 * 数据缓存
 ```javascript
 $('#id').data('参数1','参数2');// 如果只有一个参数代表取缓存里面的数据 两个参数代表设置数据第一个参数为键
+$('#id').deleteData('键');
 ```
 数据缓存一般的应用到需要对操作结果进行记忆。
 
@@ -65,7 +66,109 @@ $('#id').removeClass('class名称');// 移除class
 
 * 事件模块
 ```javascript
-// 开发中
+# touch事件
+
+## 回调函数的参数
+回调函数里面的参数event包含10个属性:pagex,pageY,clientX,clientY,screenX,screenY,direction,displacementX,displacementY,targets.
+
+screenX
+
+事件发生时手指触摸位置距离屏幕左上角的X方向(水平)距离.
+
+screenY
+
+事件发生时手指触摸位置距离屏幕左上角的Y方向(竖直)距离.
+
+clientX
+
+事件发生时手指触摸位置距离浏览器网页显示区域左上角的X方向(水平)距离.
+
+clientY
+
+事件发生时手指触摸位置距离浏览器网页显示区域左上角的Y方向(竖直)距离.
+
+pageX
+
+事件发生时手指触摸位置距离页面左上角的X方向(水平)距离.
+
+pageY
+
+事件发生时手指触摸位置距离页面左上角的Y方向(竖直)距离.
+
+direction
+
+事件发生时手指触摸的方向,如果direction.x为false代表向左滑动,为true代表向右滑动.
+
+如果direction.y为false代表向下滑动,为true代表向上滑动.
+
+displacementX
+
+手指相对于起始点的X方向(水平)移动距离.
+
+displacementY
+
+手指相对于起始点的Y方向(竖直)移动距离.
+
+targets
+
+触摸的目标元素.
+
+## tap事件
+tap事件是手指点触事件,tap事件的触发过程是手指轻点屏幕并迅速离开.
+
+```javascript
+$('#id或者.class').bind('tap',function(event){
+	// 回调方法
+});
+```
+
+## doubletap事件
+doubletap事件是用一根手指在触屏上快速连续点击两次.
+
+```javascript
+$(#id或者.class).bind('doubletap',function(event){
+	// 回调方法
+});
+```
+
+## hold事件
+hold事件又叫长按事件,用一根手指在触屏上按住0.3秒以上.
+
+```javascript
+$('#id或者.class').bind('hold',function(event){
+	// 回调方法
+});
+```
+
+## holdend事件
+holdend事件用一根手指在触屏上按住0.3秒以上,手指离开触屏时触发.
+
+```javascript
+$('#id或者.class').bind('holdend',function(event){
+	// 回调方法
+});
+```
+
+## swipe事件
+滑动事件用一根手指在触屏上滑动时触发.
+
+```javascript
+$('#id或者.class').bind('swipestart',function(e){
+  //alert('开始滑动');
+});
+$('#id或者.class').bind('swipemove',function(e){
+  //alert('滑动中');
+});
+$('#id或者.class').bind('swipeend',function(e){
+  alert('结束滑动');
+});
+
+```
+### unbind取消绑定事件
+
+```javascript
+$('#id或者.class').unbind('需要取消的事件名称');
+```
 ```
 
 ### 表单验证模块
